5fea2ebaf80efa7c99cd4cf579a358abeeb8ba36
SimonLaws <simon.laws@csiro.au>
2025-06-12 08:23:03 +1000

    adding jira sd link to feedback button

--- dashTitlev2.js
+++ dashTitlev2.js
@@ -181,7 +181,7 @@
     
     //The below code is used to create the Jira Issue collector box that pops up when the feedback button is clicked
     connectedCallback() {
-      const collectorUrl = this._feedbackLink;
+      const collectorUrl = "https://jira-sd.csiro.au/plugins/servlet/issueCollectorBootstrap.js?collectorId=57d7f85e&locale=en_AU";
 
       const button = this.shadowRoot.getElementById("feedback-button");
 
