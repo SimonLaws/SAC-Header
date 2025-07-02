a498b3395ff003c699dc32ec2326e1cbdaf240d4
SimonLaws <simon.laws@csiro.au>
2025-07-02 11:42:49 +1000

    more subtitle font

--- dashTitlev2.js
+++ dashTitlev2.js
@@ -43,7 +43,7 @@
       :host .dashSubTitle {
         font-size: 0.9375rem;
         color: #707070;
-        font-family: "Montserrat";
+        font-family: "Montserrat", sans-serif;
         font-weight: 400;
         line-height: 1.5;
       }
